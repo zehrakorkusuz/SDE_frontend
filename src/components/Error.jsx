762ac20f833b0725064fd89e274fe5fdf87ee19e
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-26 19:27:59 +0100

    login error message

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Error({ message }) {
+  if (message === "No Token given") {
+    return (
+      <div>
+        Error: you must log in
+        <br />
+        <Link to="/login">Go to login</Link>
+      </div>
+    );
+  }
+
   return <div>Error: {message}</div>;
 }
