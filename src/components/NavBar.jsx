2f07d47993c795ea62f4298eb04f902b3c23b85e
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-26 23:34:37 +0100

    easier nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,8 @@
 import React from "react";
-import { useState } from "react";
-import { FaBars } from "react-icons/fa";
-import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import LogOutButton from "./LogOutButton";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Toggle the navbar when the menu icon is clicked
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav
       style={{
@@ -21,44 +11,31 @@ function Navbar() {
         borderBottom: "1px solid black",
       }}
     >
-      <div
-        onClick={toggleNavbar}
+      <ul
         style={{
-          fontSize: "2.2rem",
-          width: "5%",
-          textAlign: "center",
-          alignItems: "center",
-          backgroundColor: "var(--primary-dark-color)",
+          width: "95%",
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "auto",
+          listStyleType: "none",
+          height: "auto",
+          fontSize: "x-large",
+          padding: "0.5%"
         }}
       >
-        {isOpen ? <AiOutlineClose /> : <FaBars />}
-      </div>
-      {isOpen && (
-        <ul
-          style={{
-            width: "95%",
-            display: "flex",
-            justifyContent: "space-around",
-            margin: "auto",
-            listStyleType: "none",
-            height: "auto",
-            fontSize: "x-large",
-          }}
-        >
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <LogOutButton />
-          </li>
-        </ul>
-      )}
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <LogOutButton />
+        </li>
+      </ul>
     </nav>
   );
 }
